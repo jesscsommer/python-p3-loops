e0eb1b906f8898c828fe8e2ca16563ddc1b8dad5
Jessica Sommerville <jessica.c.sommerville@gmail.com>
2023-05-21 20:52:08 -0700

    Complete lab

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,25 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    i = 10
+    while i > 0: 
+        print(f'{i}') 
+        i -= 1
+    print('Happy New Year!')
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    squared = [int * int for int in int_list]
+    return squared
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    i = 1
+    while i <= 100: 
+        if i % 3 == 0 and i % 5 != 0: 
+            print('Fizz')
+        elif i % 5 == 0 and i % 3 != 0: 
+            print('Buzz')
+        elif i % 3 == 0 and i % 5 == 0: 
+            print('FizzBuzz')
+        else: 
+            print(f'{i}')
+        i += 1
